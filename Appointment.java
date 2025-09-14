2735631c4256f8eab0196cfedc1213f8a3bafda6
CuongTC2610 <Cuongtran.dbp.140264@gmail.com>
2025-09-14 20:54:47 +0700

    Update Appointment.java

--- Appointment.java
+++ Appointment.java
@@ -1,1 +1,20 @@
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    private Doctor doctor;
+
+    @ManyToOne
+    private Patient patient;
+
+    private LocalDateTime appointmentTime;
+
+    // getters và setters
+}
 
