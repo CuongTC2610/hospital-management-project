c07f182689e93b1de4e7ba200f5e95f995cca949
CuongTC2610 <Cuongtran.dbp.140264@gmail.com>
2025-09-14 20:54:34 +0700

    Update Doctor.java

--- Doctor.java
+++ Doctor.java
@@ -1,1 +1,18 @@
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+public class Doctor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    private String name;
+    
+    @ElementCollection
+    private List<LocalDateTime> availableTimes;
+
+    // getters và setters
+}
 
